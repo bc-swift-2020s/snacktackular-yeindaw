3b1fff6a8dc9417406733de11c7d2e49918638a7
Dawin Ye <59813280+yeindaw@users.noreply.github.com>
2020-04-19 17:38:28 -0700

    Update
    g

--- Snacktacular/AppDelegate.swift
+++ Snacktacular/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSPlacesClient.provideAPIKey("AIzaSyDL6a2av3CG69COHyRUsJ8dyZmCe-kRI1s")
+        GMSPlacesClient.provideAPIKey("")
         FirebaseApp.configure()
         return true
     }
